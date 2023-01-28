375960c469b2c4f7482dfb5fb284855b12e40f70
kennedybabu <kennedynjogu8@gmail.com>
2023-01-28 13:49:48 +0300

    feature: create initial folder structure, and User interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user.ts
@@ -0,0 +1,5 @@
+export interface User {
+    fullname: string,
+    email:[''],
+    password: any
+}
